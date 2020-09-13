75e16f394d7e31e88285afc4ecae05dae040d3d3
Kaito Yamaki <kikngo526@gmail.com>
2020-09-13 12:18:23 +0900

    category作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,22 @@ class BooksController < ApplicationController
   def show
   end
 
-  def new 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name,:category_id)
   end
 end
