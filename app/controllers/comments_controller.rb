8c77c524541b6f96c6afd00baf99b091b3c8fd46
Kaito Yamaki <kikngo526@gmail.com>
2020-09-17 12:35:28 +0900

    削除機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,15 @@ class CommentsController < ApplicationController
     redirect_to "/books/#{comment.book.id}"
   end
 
+  def destroy
+    comment = Comment.find(params[:id])
+    if comment.destroy
+       redirect_to "/books/#{comment.book.id}"
+    else
+      render :show
+    end
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, book_id: params[:book_id])
